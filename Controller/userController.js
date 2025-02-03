4b5d229febda3819efb1de8823d47a31e170e534
LVP <lalivp11@gmail.com>
2025-02-03 14:28:55 -0500

    JWT token generation

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const { studentCollection } = require('../Models/dbschema');
 const async = require("async");
+const jwt = require('jsonwebtoken');
 
 const getUser = (req, res) => {
 
@@ -50,14 +51,44 @@ const regUser = (req, res) => {
 }
 
 const loginUser = (req, res) =>{
-    studentCollection.find({email:req.body.email}).then((data) =>{
-        if(data.length > 0){
-            res.send(data[0])
-        } else {
-            res.status(404).send({message:"email not found"})
+    async.waterfall([
+        // 1.Check the email id if already exist or not
+        (callback)=>{
+            studentCollection.find({email:req.body.email}).then((data) =>{
+                if(data.length > 0){
+                    userDetails = data[0];
+                    callback(null, userDetails)
+                } else {
+                    res.status(404).send({message:"email not found"})
+                }
+            }).catch ((error) =>{
+                callback(error);
+            })
+
+        },
+        //2. Comparing the password
+        (arg1, callback)=>{
+            bcrypt.compare(req.body.password, arg1.password, function (err, result){
+                if (result){
+                    callback(null,arg1);
+                } else {
+                    res.status(401).send({message:"Incorrect password"});
+                }
+            })
+        },
+        //3. generating the JWT token
+        (arg2,callback)=>{
+            let token = jwt.sign({email:arg2.email}, "secretkey", {expiresIn:"1h"});
+            callback (null, token);
         }
-    }).catch ((error) =>{
-        console.log(error);
+    ],(err, result)=>{
+        if(err){
+            res.send(err);
+        }else {
+            result2= {...userDetails._doc};
+            delete result2.password;
+            res.status(200).send({status:true, data: result2, token: result});
+        }
     })
 }
 
